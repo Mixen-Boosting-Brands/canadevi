884b422a37a3e3800a53cb10fc860492314e8842
Luis Carlos Pando <hey@luiscarlospando.com>
2023-08-08 16:52:19 -0600

    Avance hasta el 8/08/2023

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Libraries
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+
+// Local Scripts
+import '../src/smooth-scrolling';
+import '../src/form-ajax';
+import '../src/aos';
