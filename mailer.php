80a054b2f7246acdb06b6d4306e30db84479073f
Luis Carlos Pando <hey@luiscarlospando.com>
2023-08-10 10:20:54 -0600

    Ajustes Alfredo

--- mailer.php
+++ mailer.php
@@ -23,8 +23,8 @@
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'no-reply@alzais.com';                     //SMTP username
-    $mail->Password   = '&9WXS9BWvZ%5';                               //SMTP password
+    $mail->Username   = 'noreply@feriacanadevi.mx';                     //SMTP username
+    $mail->Password   = 'pY9pzBmTYZy.';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;
     // $mail->SMTPDebug = 1;
