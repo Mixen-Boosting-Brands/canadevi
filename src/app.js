a2ffebefb49204b2dd8d2c97be10850d875fa845
Luis Carlos Pando <hey@luiscarlospando.com>
2023-08-23 13:14:34 -0600

    Agregar banner de fraccionamientos con parallax y actualizar videos de TikTok

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
 // Local Scripts
+import '../src/parallax';
 import '../src/smooth-scrolling';
 import '../src/form-ajax';
 import '../src/aos';
